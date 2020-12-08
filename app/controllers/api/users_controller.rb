15516e5e76dfc766a56487e1491c331638c6f5e6
erikalvar <erikalvarbass@gmail.com>
2020-12-08 15:34:42 -0500

    adds user update, show, delete

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,4 +12,45 @@ class Api::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user == current_user
+      render "show.json.jb"
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user == current_user
+      @user = current_user
+      @user.name = params[:name] || @user.name
+      @user.email = params[:email] || @user.email
+
+      if params[:password]
+        @user.password = params[:password] || @user.password
+        @user.password_confirmation = params[:password_confirmation] || @user.password_confirmation
+      end
+
+      if @user.save
+        render "show.json.jb"
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { message: "User not logged in" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if @user == current_user
+      @user.destroy
+      render json: { message: "User successfully deleted." }
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :carted_products
   has_many :products, through: :carted_products
   has_many :orders, through: :carted_products
+
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true, length: { minimum: 4 }, uniqueness: true
 end
